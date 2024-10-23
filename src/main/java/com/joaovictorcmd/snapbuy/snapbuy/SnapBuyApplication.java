dbc7b3fd484caea2d84e5feac29124cdcbafb481
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2024-10-23 10:16:28 -0300

    :tada: Initial commit

---
+++ src/main/java/com/joaovictorcmd/snapbuy/snapbuy/SnapBuyApplication.java
@@ -0,0 +1,13 @@
+package com.joaovictorcmd.snapbuy.snapbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapBuyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnapBuyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joaovictorcmd/snapbuy/snapbuy/SnapBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovictorcmd.snapbuy.snapbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapBuyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
